5203ad71ed3adfa8476333f3c249f2ee1a80459a
stackinspector <stackinspector@126.com>
2021-08-15 00:11:44 +0800

    feat: basic tiny-http redirect server

--- src/bin/http-redirector.rs
+++ src/bin/http-redirector.rs
@@ -1,6 +1,7 @@
 use structopt::StructOpt;
 use reqwest::blocking::get;
-use http_redirector::{parse_map};
+use tiny_http::{Header, Response, Server};
+use http_redirector::parse_map;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "http-redirector")]
@@ -18,5 +19,17 @@ fn main() {
     println!("port = {}", args.port);
     for (key, val) in config {
         println!("{} => {}", key, val);
+    }
+    let server = Server::http(format!("0.0.0.0:{}", args.port)).unwrap();
+    
+    for request in server.incoming_requests() {
+        let header: Header = format!(
+            "Location: https://cn.bing.com/search?ensearch=1&q={}",
+            request.url()
+        )
+        .parse::<Header>()
+        .unwrap();
+        let response = Response::empty(301).with_header(header);
+        request.respond(response).unwrap();
     }
 }
