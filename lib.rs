24ed365f88b08986adf95812f847d9b68359d812
stackinspector <stackinspector@126.com>
2022-01-31 06:16:40 +0800

    refactor

--- lib.rs
+++ lib.rs
@@ -1,5 +1,6 @@
-use std::{collections::HashMap, sync::Arc, net::SocketAddr, io::Write};
+use std::{collections::HashMap, sync::Arc, net::SocketAddr, io::{self, Write}, fs, path::PathBuf};
 use tokio::{spawn, sync::mpsc::{unbounded_channel, UnboundedSender as Sender}};
+pub use serde_json::Value as JsonValue;
 use warp::{http::Response, hyper::Body};
 
 type Map = HashMap<String, String>;
@@ -9,7 +10,9 @@ pub fn now() -> u64 {
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis().try_into().unwrap()
 }
 
-pub fn build_client() -> hyper::Client<hyper_rustls::HttpsConnector<hyper::client::connect::HttpConnector>> {
+type HttpClient = hyper::Client<hyper_rustls::HttpsConnector<hyper::client::connect::HttpConnector>>;
+
+pub fn build_client() -> HttpClient {
     let connector = hyper_rustls::HttpsConnectorBuilder::new()
         .with_webpki_roots()
         .https_only()
@@ -21,19 +24,20 @@ pub fn build_client() -> hyper::Client<hyper_rustls::HttpsConnector<hyper::clien
 #[derive(serde::Serialize)]
 pub struct Init {
     pub time: u64,
+    pub ver: String,
     pub url: String,
+    pub map: JsonValue,
 }
 
 #[derive(serde::Serialize, Debug)]
-pub struct Record {
+pub struct Event {
     pub time: u64,
+    pub from: Vec<String>,
     pub key: String,
     pub hit: bool,
-    pub ip: Option<String>,
-    pub xff: Option<Vec<String>>,
 }
 
-pub async fn get(url: &str) -> Option<String> {
+async fn get(url: &str) -> Option<String> {
     if url.starts_with("http") {
         let client = build_client();
         let resp = client.get(url.parse().ok()?).await.ok()?;
@@ -48,7 +52,7 @@ pub async fn get(url: &str) -> Option<String> {
     }
 }
 
-pub fn init(config: &str, map: &mut Map) -> Option<()> {
+fn init_map(config: &str, map: &mut Map) -> Option<()> {
     // map.clear();
     for line in config.lines().filter(|s| s.len() != 0) {
         let mut splited = line.split(' ').filter(|s| s.len() != 0);
@@ -66,29 +70,30 @@ pub fn init(config: &str, map: &mut Map) -> Option<()> {
 }
 
 pub async fn handle(
-    key: String, ip: Option<SocketAddr>, xff: Option<String>, map: Arc<Map>, log_sender: Sender<Record>
+    key: String, ip: Option<SocketAddr>, xff: Option<String>, map: Arc<Map>, log_sender: Sender<Event>
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    let time = now();
+    let mut from = Vec::new();
+    if let Some(ip) = ip {
+        from.push(ip.to_string())
+    };
+    if let Some(xff) = xff {
+        for _ip in xff.split(',') {
+            from.push(_ip.to_owned())
+        }
+    };
     let result = map.get(&key);
-    log_sender.send(Record {
-        time: now(),
-        key,
-        hit: result.is_some(),
-        ip: ip.map(|val| val.to_string()),
-        xff: xff.map(|val| val.split(',').map(|s| s.to_owned()).collect()),
-    }).unwrap();
+    let hit = result.is_some();
+    log_sender.send(Event { time, from, key, hit }).unwrap();
     Ok(match result {
         None => Response::builder().status(404).body(Body::empty()).unwrap(),
         Some(val) => Response::builder().status(307).header("Location", val).body(Body::empty()).unwrap(),
     })
 }
 
-pub fn log_thread<W: Write + 'static + Send>(url: String, mut writer: W) -> Sender<Record> {
-    let (tx, mut rx) = unbounded_channel::<Record>();
+pub fn log_thread<W: Write + 'static + Send>(init: Init, mut writer: W) -> Sender<Event> {
+    let (tx, mut rx) = unbounded_channel::<Event>();
     spawn(async move {
-        let init = Init {
-            time: now(),
-            url,
-        };
         serde_json::to_writer(&mut writer, &init).unwrap();
         writeln!(writer).unwrap();
         while let Some(record) = rx.recv().await {
@@ -97,6 +102,23 @@ pub fn log_thread<W: Write + 'static + Send>(url: String, mut writer: W) -> Send
         }
     });
     tx
+}
+
+pub async fn init(url: String, log_path: Option<PathBuf>) -> (Map, Sender<Event>) {
+    let mut map = HashMap::new();
+    let config = get(url.as_str()).await.unwrap();
+    init_map(config.as_str(), &mut map).unwrap();
+    let init = Init {
+        time: now(),
+        ver: env!("CARGO_PKG_VERSION").to_owned(),
+        url,
+        map: serde_json::to_value(&map).unwrap(),
+    };
+    let log_sender = match log_path {
+        Some(path) => log_thread(init, fs::OpenOptions::new().write(true).create(true).append(true).open(path).unwrap()),
+        None => log_thread(init, io::stdout()),
+    };
+    (map, log_sender)
 }
 
 #[cfg(test)]
@@ -112,7 +134,7 @@ trpl-cn kaisery.github.io/trpl-zh-cn/
 
     fn wrapped_init(config: &str) -> Option<Map> {
         let mut map = HashMap::new();
-        init(config, &mut map)?;
+        init_map(config, &mut map)?;
         Some(map)
     }
 
--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashMap, sync::Arc, io, fs, path::PathBuf};
+use std::{sync::Arc, path::PathBuf};
 use tokio::{spawn, signal, sync::oneshot};
 use structopt::StructOpt;
 use warp::Filter;
-use http_redirector::{get, init, handle, log_thread};
+use http_redirector::{init, handle};
 
 #[derive(StructOpt)]
 #[structopt(about = concat!(env!("CARGO_PKG_DESCRIPTION"), "\nsee https://github.com/stackinspector/http-redirector"))]
@@ -19,18 +19,10 @@ struct Args {
 async fn main() {
     let Args { port, url, log_path } = Args::from_args();
 
-    let map = Arc::new({
-        let mut _map = HashMap::new();
-        let config = get(url.as_str()).await.unwrap();
-        init(config.as_str(), &mut _map).unwrap();
-        _map
-    });
-    let map_filter = warp::any().map(move || map.clone());
+    let (map, log_sender) = init(url, log_path).await;
+    let map = Arc::new(map);
 
-    let log_sender = match log_path {
-        Some(path) => log_thread(url, fs::OpenOptions::new().write(true).create(true).append(true).open(path).unwrap()),
-        None => log_thread(url, io::stdout()),
-    };
+    let map_filter = warp::any().map(move || map.clone());
     let log_sender_filter = warp::any().map(move || log_sender.clone());
 
     let (tx, rx) = oneshot::channel();
@@ -44,7 +36,7 @@ async fn main() {
         .and_then(handle);
 
     let (_addr, server) = warp::serve(route).bind_with_graceful_shutdown(
-        ([127, 0, 0, 1], port),
+        ([0, 0, 0, 0], port),
         async { rx.await.unwrap(); }
     );
 
