ef75d49136f0b1c75666dfae04031d1e70ec4027
stackinspector <stackinspector@126.com>
2023-10-27 04:07:10 +0800

    http client in Context & Arc<Context>

--- lib.rs
+++ lib.rs
@@ -1,7 +1,8 @@
 use std::{collections::HashMap, io::{self, Write}, fs, path::PathBuf, net::SocketAddr, sync::{Arc, OnceLock}, borrow::Cow, convert::Infallible};
 use serde::Serialize;
 use tokio::sync::RwLock;
-use hyper::{http::{Request, Response, header, Method, StatusCode}, Body};
+use hyper::{http::{Request, Response, header, Method, StatusCode}, Body, client::{Client, HttpConnector}};
+use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
 
 const UPDATE_PATH_STR: &str = "__update__";
 
@@ -75,22 +76,20 @@ pub enum UpdateResult {
     ParseConfigError,
 }
 
-type HttpClient = hyper::Client<hyper_rustls::HttpsConnector<hyper::client::connect::HttpConnector>>;
-
-const HTTP_CLIENT: OnceLock<HttpClient> = OnceLock::new();
+type HttpClient = Client<HttpsConnector<HttpConnector>>;
 
 fn build_http_client() -> HttpClient {
-    let connector = hyper_rustls::HttpsConnectorBuilder::new()
+    let connector = HttpsConnectorBuilder::new()
         .with_webpki_roots()
         .https_only()
         .enable_http1()
         .build();
-    hyper::Client::builder().build(connector)
+    Client::builder().build(connector)
 }
 
-async fn get(url: &str) -> anyhow::Result<String> {
+async fn get(slot: &OnceLock<HttpClient>, url: &str) -> anyhow::Result<String> {
     if url.starts_with("http") {
-        let resp = HTTP_CLIENT.get_or_init(build_http_client).get(url.parse()?).await?;
+        let resp = slot.get_or_init(build_http_client).get(url.parse()?).await?;
         let status = resp.status().as_u16();
         if status == 200 {
             let bytes = hyper::body::to_bytes(resp.into_body()).await?;
@@ -159,11 +158,11 @@ fn init_map(config: &str) -> Option<HashMap<String, String>> {
 
 // TODO split(char) or split(str)?
 
-#[derive(Clone)]
 pub struct Context {
-    state_ref: StateRef,
+    state_ref: RwLock<State>,
     log_sender: tokio_actor::Handle<LogContext>,
-    req_id_header: Option<Arc<str>>,
+    req_id_header: Option<String>,
+    http_client: OnceLock<HttpClient>,
     allow_update: bool,
 }
 
@@ -173,8 +172,8 @@ impl Context {
         log_path: Option<PathBuf>,
         req_id_header: Option<String>,
         allow_update: bool,
-    ) -> anyhow::Result<(Context, LogCloser)> {
-        let req_id_header = req_id_header.map(Arc::from);
+    ) -> anyhow::Result<(Arc<Context>, LogCloser)> {
+        let http_client = OnceLock::new();
         let mut state = HashMap::new();
         for pair in input.split(';') {
             let (scope_name, url) = split_kv(pair.split(',')).ok_or_else(|| {
@@ -183,7 +182,7 @@ impl Context {
             if scope_name == UPDATE_PATH_STR {
                 return Err(anyhow::anyhow!("scope name should not be \"{}\"", UPDATE_PATH_STR))
             }
-            let config = get(url).await?;
+            let config = get(&http_client, url).await?;
             let map = init_map(config.as_str()).ok_or_else(|| {
                 anyhow::anyhow!("parsing config \"{}\" error", url)
             })?;
@@ -203,19 +202,20 @@ impl Context {
         } };
         // TODO should wait for log writed? (actor's default behavior)
         log_sender.request(serde_json::to_string(&event).unwrap()).await?;
-        Ok((Context {
-            state_ref: Arc::new(RwLock::new(state)),
+        Ok((Arc::new(Context {
+            state_ref: RwLock::new(state),
             log_sender: log_sender.clone(),
             req_id_header,
+            http_client,
             allow_update,
-        }, LogCloser {
+        }), LogCloser {
             handle: log_sender
         }))
     }
 
-    pub async fn handle(self, remote_addr: SocketAddr, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    pub async fn handle(self: Arc<Self>, remote_addr: SocketAddr, req: Request<Body>) -> Result<Response<Body>, Infallible> {
         // TODO if Err is not ! then empty response??
-        let Context { state_ref, log_sender, req_id_header, allow_update } = self;
+        let Context { state_ref, log_sender, req_id_header, http_client, allow_update } = self.as_ref();
         let resp = Response::builder();
 
         macro_rules! err {
@@ -248,7 +248,7 @@ impl Context {
         let xff = header!("X-Forwarded-For");
         let ua = header!("User-Agent");
         let req_id = match req_id_header {
-            Some(k) => header!(k.as_ref()),
+            Some(k) => header!(k),
             None => None,
         };
 
@@ -260,13 +260,13 @@ impl Context {
             }
         };
         from.push(Cow::Owned(remote_addr.to_string()));
-        let (resp, event) = if allow_update && (scope == UPDATE_PATH_STR) {
+        let (resp, event) = if *allow_update && (scope == UPDATE_PATH_STR) {
             let mut state_ref = state_ref.write().await;
             let scope = key;
             let result = match state_ref.get(scope) {
                 None => UpdateResult::ScopeNotFound,
                 Some(scope_state) => {
-                    match get(&scope_state.url).await {
+                    match get(http_client, &scope_state.url).await {
                         Err(error) => UpdateResult::GetConfigError(error.to_string()),
                         Ok(config) => {
                             match init_map(config.as_str()) {
